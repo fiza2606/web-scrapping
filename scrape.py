a484c4d13dccb3ee78e9f89c8402eb3c8c6cf1ed
fiza2606 <45958068+fiza2606@users.noreply.github.com>
2021-08-21 20:24:28 +0530

    Add files via upload

---
+++ scrape.py
@@ -0,0 +1,17 @@
+import requests
+import MySQLdb
+from bs4 import BeautifulSoup
+
+#SQL connection data to connect and save the data in
+HOST = "localhost"
+USERNAME = "scraping_user"
+PASSWORD = ""
+DATABASE = "scraping_sample"
+
+#URL to be scraped
+url_to_scrape = 'https://howpcrules.com/sample-page-for-web-scraping/'
+#Load html's plain data into a variable
+plain_html_text = requests.get(url_to_scrape)
+#parse the data
+soup = BeautifulSoup(plain_html_text.text, "html.parser")
+print(soup.prettify())
